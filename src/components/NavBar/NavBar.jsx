47eef903ba26066854c3ef41b912e5fca84e43da
Dusan Ilic <dule979@gmail.com>
2024-04-25 19:34:21 +0200

    Adding Navbar component.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,71 @@
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from '@mui/material';
+import {
+  Menu,
+  AccountCircle,
+  Brightness7,
+  Brightness4,
+} from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/styles';
+
+import useStyles from './styles';
+
 function NavBar() {
-  console.log('NavBar');
-  return <div>NavBar</div>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width: 600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
+
+  return (
+    <AppBar position="fixed">
+      <Toolbar className={classes.toolbar}>
+        {isMobile && (
+          <IconButton
+            color="inherit"
+            edge="start"
+            style={{ outline: 'none' }}
+            onClick={() => {}}
+            className={classes.menuButton}
+          >
+            <Menu />
+          </IconButton>
+        )}
+        <IconButton color="inherit" onClick={() => {}} sx={{ ml: 1 }}>
+          {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
+        {!isMobile && 'Search...'}
+        <div>
+          {!isAuthenticated ? (
+            <Button color="inherit" onClick={() => {}}>
+              Login &nbsp; <AccountCircle />
+            </Button>
+          ) : (
+            <Button
+              color="inherit"
+              onClick={() => {}}
+              component={Link}
+              to="/profile/:id"
+              className={classes.linkButton}
+            >
+              {!isMobile && <>My movies &nbsp;</>}
+              <Avatar
+                style={{ width: '30px', height: '30px' }}
+                src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                alt="profile"
+              />
+            </Button>
+          )}
+        </div>
+        {isMobile && 'Search...'}
+      </Toolbar>
+    </AppBar>
+  );
 }
 export default NavBar;
